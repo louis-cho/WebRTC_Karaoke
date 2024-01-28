759332fdbe3423f659b230f4239f4356331af3ea
연정흠 <madirony52@kakao.com>
2024-01-28 23:58:20 +0900

    [BE feat] STOMP pub/sub Test

---
+++ server/src/main/java/com/ssafy/server/chat/model/ChatMessageDTO.java
@@ -0,0 +1,13 @@
+package com.ssafy.server.chat.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessageDTO {
+
+    private String roomId;
+    private String writer;
+    private String message;
+}
