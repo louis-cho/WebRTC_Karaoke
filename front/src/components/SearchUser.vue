a320687010626b68f755993e07aaad61bc8c61a4
sungeun <seroh00@naver.com>
2024-01-29 08:53:44 +0900

    FE SearchUser 컴포넌트 생성

--- front/src/components/SearchUser.vue
+++ front/src/components/SearchUser.vue
@@ -12,11 +12,18 @@
 
         <!-- card-section 부분 수정해야함 -->
         <q-card-section class="q-pt-none">
-          <q-input rounded outlined v-model="password" label="비밀번호" />
+          <q-input rounded outlined v-model="search" label="비밀번호" >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+          </q-input>
         </q-card-section>
-
+      <div>
+        <!-- 친구들 목록 뜨게 -->
+      </div>
       </q-card>
     </q-dialog>
+
   </div>
 </template>
 
@@ -28,5 +35,59 @@ const icon = ref(false)
 </script>
 
 <style scoped>
+.width-5 {
+  width: 5%;
+}
+.width-10 {
+  width: 10%;
+}
+.width-15 {
+  width: 15%;
+}
+.width-20 {
+  width: 20%;
+}
+.width-25 {
+  width: 25%;
+}
+.width-30 {
+  width: 30%;
+}
+.width-35 {
+  width: 35%;
+}
+.width-40 {
+  width: 40%;
+}
+.width-45 {
+  width: 45%;
+}
+.width-50 {
+  width: 50%;
+}
+.width-55 {
+  width: 55%;
+}
+.width-60 {
+  width: 60%;
+}.width-65 {
+  width: 65%;
+}.width-70 {
+  width: 70%;
+}.width-75 {
+  width: 75%;
+}.width-80 {
+  width: 80%;
+}.width-85 {
+  width: 85%;
+}.width-90 {
+  width: 90%;
+}.width-95 {
+  width: 95%;
+}.width-100 {
+  width: 100%;
+}
+
+
 
 </style>
