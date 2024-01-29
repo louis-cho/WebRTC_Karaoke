b4f734ba1614ee6656bab7227e09645f9834e046
louis-cho <wizardchw@naver.com>
2024-01-29 09:50:05 +0900

    [FE Feat] frontend 앱 string literal 설정 preference 구현

---
+++ front/src/js/config/preference.js
@@ -0,0 +1,21 @@
+let app = {};
+
+app.kor = {};
+
+app.kor.mainPage = {};
+app.kor.mainPage.title = "노래해방";
+app.kor.mainPage.content = "실시간 노래방 서비스";
+
+app.kor.loginPage = {};
+app.kor.loginPage.title = "로그인";
+app.kor.loginPage.idLabel = "id";
+app.kor.loginPage.idHint = "id를 입력해주세요";
+app.kor.loginPage.pwLabel = "pw";
+app.kor.loginPage.pwHint = "pw를 입력해주세요";
+
+app.eng = {};
+
+app.eng.mainPage = {};
+app.eng.mainPage.title = "Noraehaebang";
+
+export default { app };
---
+++ front/src/js/config/testPreference.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <div>
+    <h3 id="mainPage_title"></h3>
+    <h6 id="mainPage_content"></h6>
+  </div>
+
+    <TabItem/>
+
+    <!-- 로그인 전에 시작하기 뜨는 navbar -->
+    <TabItemStart/>
+  </div>
+
+</template>
+
+<script setup>
+import TabItemStart from "@/layouts/TabItemStart.vue";
+import TabItem from "@/layouts/TabItem.vue";
+
+import { ref, onMounted } from "vue";
+import * as pref from "@/js/config/preference.js";
+
+function init(lang) {
+
+    document.getElementById("mainPage_title").innerHTML = pref.default.app[lang].mainPage.title;
+    document.getElementById("mainPage_content").innerHTML = pref.default.app[lang].mainPage.content;
+}
+
+onMounted(() => {
+  init("kor");
+});
+
+</script>
+
+<style scoped>
+</style>
