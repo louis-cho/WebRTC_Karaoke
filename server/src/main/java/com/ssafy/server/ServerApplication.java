44a8c62f3683d7ddf1691f4106a77a6b4bbb78d5
joonbum <1996joon@naver.com>
2024-01-29 17:45:07 +0900

    feat: point api생성. entity 슈퍼클래스 생성. SpringApplication에 어노테이션 추가

--- server/src/main/java/com/ssafy/server/ServerApplication.java
+++ server/src/main/java/com/ssafy/server/ServerApplication.java
@@ -2,11 +2,22 @@ package com.ssafy.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ServerApplication {
 
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
