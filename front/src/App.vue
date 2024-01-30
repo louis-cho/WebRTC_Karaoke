bf86635db1098d96082c2bf1fd534360bb1a6300
chohyunwoo <wizardchw@naver.com>
2024-01-30 21:02:40 +0900

    [FE Fix] App.vue fix

--- front/src/App.vue
+++ front/src/App.vue
@@ -4,17 +4,8 @@
   </div>
 </template>
 
-<<<<<<< HEAD
-<script setup>
-
-</script>
-
-<style scoped>
-
-=======
 <script setup></script>
 
 <style scoped>
 /* 스타일링 추가 */
->>>>>>> e9c9979a6ae91e42fc76e89b8dbc00f52ac2181a
 </style>
