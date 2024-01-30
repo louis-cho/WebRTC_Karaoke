4deb7da743623cf26070246f5793a51adc9d43eb
joonbum <1996joon@naver.com>
2024-01-31 01:10:18 +0900

    feat : deploy test

--- server/src/main/java/com/ssafy/server/OpenViduAPI.java
+++ server/src/main/java/com/ssafy/server/OpenViduAPI.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import java.util.Map;
 
-@CrossOrigin(origins = "*")
+
 @RestController
 public class OpenViduAPI {
 
--- server/src/main/java/com/ssafy/server/config/CorsConfig.java
+++ server/src/main/java/com/ssafy/server/config/CorsConfig.java
@@ -13,10 +13,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 모든 origin 허용
+//                .allowedOriginPatterns("") // 모든 origin 허용
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
-                .allowCredentials(true)
+//                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
