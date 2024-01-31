ef4f616dc570193eed79645034e2f687499d7ad2
jsong98 <jswsn1526@gmail.com>
2024-01-31 16:08:19 +0900

    fix: move s3FileUploader

--- server/src/main/java/com/ssafy/server/common/util/S3FileUploader.java
+++ server/src/main/java/com/ssafy/server/common/util/S3FileUploader.java
@@ -1,4 +1,4 @@
-package com.ssafy.server.user.util;
+package com.ssafy.server.common.util;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
--- server/src/main/java/com/ssafy/server/feed/controller/FileUploadController.java
+++ server/src/main/java/com/ssafy/server/feed/controller/FileUploadController.java
@@ -1,11 +1,8 @@
 package com.ssafy.server.feed.controller;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.ssafy.server.user.util.S3FileUploader;
+import com.ssafy.server.common.util.S3FileUploader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
