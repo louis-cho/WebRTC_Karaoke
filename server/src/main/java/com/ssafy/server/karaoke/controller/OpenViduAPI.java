b8abc16ea0c825c6b8d685f74770527feafc5aaf
joonbum <1996joon@naver.com>
2024-02-04 23:41:55 +0900

    feat: DEBUGGING을 위한 OpenViduAPI.java에 sysout추가

--- server/src/main/java/com/ssafy/server/karaoke/controller/OpenViduAPI.java
+++ server/src/main/java/com/ssafy/server/karaoke/controller/OpenViduAPI.java
@@ -36,6 +36,7 @@ public class OpenViduAPI {
         SessionProperties properties = SessionProperties.fromJson(params).build();
         // OpenVidu 객체를 사용하여 세션을 생성합니다.
         Session session = openViduModel.getOpenvidu().createSession(properties);
+        System.out.println("세션 생성 완료. session :"+session);
         // 생성된 세션의 ID를 반환합니다.
         return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
     }
@@ -51,16 +52,19 @@ public class OpenViduAPI {
     public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
                                                    @RequestBody(required = false) Map<String, Object> params)
             throws OpenViduJavaClientException, OpenViduHttpException {
+        System.out.println("지정된 세션에 새로운 연결을 생성해볼까요. sessionId : "+sessionId);
         // 지정된 세션 ID를 사용하여 활성 세션을 가져옵니다.
         Session session = openViduModel.getOpenvidu().getActiveSession(sessionId);
         // 만약 세션이 존재하지 않으면 NOT_FOUND 상태 코드를 반환합니다.
         if (session == null) {
+            System.out.println("세션이 없어요~!");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         // 전달받은 파라미터를 사용하여 연결 속성을 생성합니다.
         ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
         // 세션 객체를 사용하여 연결을 생성하고 해당 연결에 대한 토큰을 반환합니다.
         Connection connection = session.createConnection(properties);
+        System.out.println("연결 생성 완료. connection :"+connection);
         return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
     }
 }
