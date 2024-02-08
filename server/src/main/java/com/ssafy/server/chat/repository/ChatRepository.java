a9e89be0d6dcadf2fc6e4a6d0a912da969ea53b2
연정흠 <madirony52@kakao.com>
2024-02-08 13:11:58 +0900

    [BE feat] Old Chat 페이지네이션 구현

--- server/src/main/java/com/ssafy/server/chat/repository/ChatRepository.java
+++ server/src/main/java/com/ssafy/server/chat/repository/ChatRepository.java
@@ -3,11 +3,11 @@ package com.ssafy.server.chat.repository;
 import com.ssafy.server.chat.model.Chat;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-    @Query("SELECT c FROM chat c WHERE c.roomId = :roomId AND DATE(c.time) = (SELECT MAX(DATE(c2.time)) FROM chat c2 WHERE c2.roomId = :roomId)")
-    List<Chat> findLatestChatsByRoomId(@Param("roomId") String roomId);
+//    @Query("SELECT c FROM chat c WHERE c.roomId = :roomId AND DATE(c.time) = (SELECT MAX(DATE(c2.time)) FROM chat c2 WHERE c2.roomId = :roomId)")
+//    List<Chat> findLatestChatsByRoomId(@Param("roomId") String roomId);
+    List<Chat> findByRoomIdOrderByTimeDesc(@Param("roomId") String roomId);
 }
