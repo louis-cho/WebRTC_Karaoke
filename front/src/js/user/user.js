7d5616bc1ab783b8a3f36e57bf089a57fcebb740
ChoHyunWoo <wizardchw0@gmail.com>
2024-02-11 22:54:17 +0900

    [FE Feat] 유저 검색 기본 정보 (닉네임) fetch 완료

--- front/src/js/user/user.js
+++ front/src/js/user/user.js
@@ -2,25 +2,34 @@ import app from "../config/preference.js";
 
 let pref = app;
 
-
 export async function fetchUser(userPk) {
-  const serverUrl = pref.app.api.protocol + pref.app.api.host + pref.app.api.user.fetch + userPk;
+  const serverUrl =
+    pref.app.api.protocol +
+    pref.app.api.host +
+    pref.app.api.user.fetch +
+    userPk;
 
   return await fetch(serverUrl, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   })
-  .then(response => response.json())
-  .then(result => {
+    .then((response) => response.json())
+    .then((result) => {
       return result;
     });
 }
 
 // ----------------
-export async function updateUser(userKey, nickname, profileImgUrl, introduction) {
-  const serverUrl = pref.app.api.protocol + pref.app.api.host + pref.app.api.user.update;
+export async function updateUser(
+  userKey,
+  nickname,
+  profileImgUrl,
+  introduction
+) {
+  const serverUrl =
+    pref.app.api.protocol + pref.app.api.host + pref.app.api.user.update;
 
   const data = {
     userKey: userKey,
@@ -30,19 +39,37 @@ export async function updateUser(userKey, nickname, profileImgUrl, introduction)
   };
 
   return await fetch(serverUrl, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   })
-  .then(response => response.json())
-  .then(result => {
-    console.log(result)
-    return result;
-  })
-  .catch(error => {
-    console.error('개인정보수정', error);
-  });
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result);
+      return result;
+    })
+    .catch((error) => {
+      console.error("개인정보수정", error);
+    });
 }
 
+export async function searchUser(nickname) {
+  const serverUrl =
+    pref.app.api.protocol +
+    pref.app.api.host +
+    pref.app.api.user.search +
+    nickname;
+
+  return await fetch(serverUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      return result;
+    });
+}
