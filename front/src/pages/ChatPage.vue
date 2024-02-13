70330e799d8d8e8c6c42c184b2c162dd3738eda8
연정흠 <madirony52@kakao.com>
2024-02-13 14:52:56 +0900

    [FE fix] DM ChatPage 타이핑 메시지 수정

--- front/src/pages/ChatPage.vue
+++ front/src/pages/ChatPage.vue
@@ -29,7 +29,7 @@
             <div class="dm-messages" ref="messagesContainer" @scroll="handleScroll">
               <div v-for="(message, index) in messages" :key="index">
                 <div :class="['message', message.sender == userPk ? 'my-message' : 'other-message']">
-                  <template v-if="message.sender !== userPk">
+                  <template v-if="message.sender != userPk">
                     {{ getNickname(message.sender) }}
                   </template>
                   <div v-if="message.type === 'TALK' || message.type === 'TYPE'">
@@ -47,7 +47,7 @@
 
             <!-- 메시지 입력창 -->
             <div class="img_class1">
-              <textarea v-model="newMessage" @keydown.enter.prevent="sendMessage" @change="sendTyping" placeholder="메시지를 입력하세요..."></textarea>
+              <textarea v-model="newMessage" @keydown.enter.prevent="sendMessage" @input="sendTypingHandler" placeholder="메시지를 입력하세요..."></textarea>
               <label for="fileInput" class="img_label">
                 <img src="@/assets/icon/image.png" alt="File Icon" class="img_class2">
               </label>
@@ -187,12 +187,14 @@ function setTemporaryMessage(sender, type, message, time) {
   const temporaryMessage = { sender, type, message, time, temporary: true };
 
   // Add the new TYPE message to the messages array
-  messages.value.push(temporaryMessage);
+  if(userPk.value != sender){
+    messages.value.push(temporaryMessage);
 
-  // Set the timer for the TYPE message
-  temporaryMessage.timer = setTimeout(() => {
-    removeTemporaryMessage(sender, type);
-  }, 5000); // Adjust the time as needed (e.g., 5000 milliseconds for 5 seconds)
+    // Set the timer for the TYPE message
+    temporaryMessage.timer = setTimeout(() => {
+      removeTemporaryMessage(sender, type);
+    }, 5000); // Adjust the time as needed (e.g., 5000 milliseconds for 5 seconds)
+  }
 }
 
 
@@ -276,6 +278,21 @@ const stompClient = ref(null);
 
 let page = 1; // 초기 페이지 설정
 let loading = false; // 페이지 로딩 상태
+
+let typingTimer = null;
+const throttleTime = 500; // 2초
+
+const throttleSendTyping = function() {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(() => {
+    sendTyping();
+  }, throttleTime);
+};
+
+const sendTypingHandler = function() {
+  throttleSendTyping();
+};
+
 
 const sendMessage = function() {
   if (newMessage.value.trim() !== "") {
