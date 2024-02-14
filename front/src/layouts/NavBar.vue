028ab020e3c37c61e318494cc773e4268e3437e0
고정원 <7704wjddnjs@naver.com>
2024-02-14 16:15:24 +0900

    [feat] NavBar 로그인 반응형 처리

--- front/src/layouts/NavBar.vue
+++ front/src/layouts/NavBar.vue
@@ -1,26 +1,17 @@
 <template>
   <div>
     <!-- 로그인이 되어있다면 -->
-    <!-- <div v-if="isLoggedIn"> -->
-      <TabItem />
-    <!-- </div> -->
+    <TabItem v-if="store.isLoggedIn" />
     <!-- 로그인이 안되어있다면 -->
-    <!-- <div v-else> -->
-      <!-- <TabItemStart/> -->
-      <SignIn />
-    </div>
-
-  <!-- </div> -->
+    <SignIn v-else />
+  </div>
 </template>
 
 <script setup>
-// import { isLoggedIn } from "@/js/encrypt/authRequest.js";
-import TabItemStart from "@/layouts/TabItemStart.vue";
 import TabItem from "@/layouts/TabItem.vue";
 import SignIn from "@/components/SignIn.vue";
-
+import { useKaraokeStore } from "@/stores/karaokeStore.js";
+const store = useKaraokeStore();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
