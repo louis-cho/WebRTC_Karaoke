ca54cee22d8fc987b9efd14f48156261261a18e4
연정흠 <madirony52@kakao.com>
2024-02-15 16:11:04 +0900

    [DM] DM css

--- front/src/js/config/preference.js
+++ front/src/js/config/preference.js
@@ -8,11 +8,11 @@ app.api.socket = "wss:"; //ec2
 app.api.protocol = "https:";
 app.api.host = "//i10a705.p.ssafy.io" + app.api.prefix; // ec2
 app.api.websocket = app.api.socket + "//i10a705.p.ssafy.io";
+
 // app.api.socket = "ws:";
 // app.api.protocol = "http:";
 // app.api.host = "//localhost:8081" + app.api.prefix;
 // app.api.websocket = app.api.socket + "//localhost:8081";
-
 app.api.user = {};
 app.api.user.login = "/user/login";
 app.api.user.register = "/user/register";
